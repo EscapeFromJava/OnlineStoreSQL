1864813b0245b9c093808cb4004cf67a14fc04ea
Artur <reske.artur@gmail.com>
2022-05-12 18:32:34 +0300

    v 4.3 Fix 'Statistics'

--- src/main/java/com/onlinestore/onlinestoresql/controller/StatisticsController.java
+++ src/main/java/com/onlinestore/onlinestoresql/controller/StatisticsController.java
@@ -22,10 +22,7 @@ import static com.onlinestore.onlinestoresql.model.requestsSQL.Select.runSQLSele
 public class StatisticsController {
     @FXML
     BarChart barChartOrders;
-    @FXML
-    ComboBox comboBoxMonth, comboBoxYear;
     ObservableList<Order> obsListOrders;
-
     XYChart.Series<String, Number> dataSeries;
     Connection conn;
 
@@ -33,41 +30,6 @@ public class StatisticsController {
         conn = MainApplication.conn;
         obsListOrders = runSQLSelectOrders(conn);
         initBarChartOrders();
-        initComboBoxMonth();
-        initComboBoxYear();
-    }
-
-    private void initComboBoxYear() {
-        ObservableList<Order> obsListYears = FXCollections.observableArrayList(obsListOrders);
-        obsListYears.stream().map(Order::getOrder_date).collect(Collectors.toList()).forEach(el -> el.substring(0, 4));
-
-        comboBoxYear.setItems(obsListYears);
-    }
-
-    private void initComboBoxMonth() {
-        Map<Integer, String> mapMonths = new HashMap<>();
-        mapMonths.put(1, "January");
-        mapMonths.put(2, "February");
-        mapMonths.put(3, "March");
-        mapMonths.put(4, "April");
-        mapMonths.put(5, "May");
-        mapMonths.put(6, "June");
-        mapMonths.put(7, "July");
-        mapMonths.put(8, "August");
-        mapMonths.put(9, "September");
-        mapMonths.put(10, "October");
-        mapMonths.put(11, "November");
-        mapMonths.put(12, "December");
-
-        ObservableList<String> obsListMonths = FXCollections.observableArrayList();
-        obsListMonths.addAll("January", "February", "March", "April",
-                "May", "June", "July", "August",
-                "September", "October", "November", "December");
-
-        comboBoxMonth.setItems(obsListMonths);
-        comboBoxMonth.setOnAction(actionEvent -> {
-            String selectMonth = String.valueOf(comboBoxMonth.getValue());
-        });
     }
 
     private void initBarChartOrders() {
